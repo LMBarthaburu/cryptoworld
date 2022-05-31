72038a409cc9522d75e3f0b9bcb21372a67dbf05
Luis Miguel Barthaburu <barthaburuluismiguel@outlook.com>
2022-05-31 20:08:17 -0300

    cambio en seccion favoritos

--- src/pages/Favoritos/Favoritos.js
+++ src/pages/Favoritos/Favoritos.js
@@ -22,8 +22,9 @@ function Favoritos() {
       <div className='container favoritos'>
         <h1 className='my-3'>Monedas Favoritas:</h1>
         <div className='row'>
-          {favoritos? 
-          favoritos.map(favorito=><CardFavoritos key={favorito.id} ranking={favorito.market_cap_rank} img={favorito.image} nombre={favorito.name} simbolo={favorito.symbol} precio={favorito.current_price} cambio={favorito.price_change_percentage_24h} capitalizacion={favorito.market_cap} id={favorito.id}/>) : <p className='fav-text'>No tienes favoritos Seleccionados</p> 
+          {favoritos && favoritos.length>0? 
+          favoritos.map(favorito=><CardFavoritos key={favorito.id} ranking={favorito.market_cap_rank} img={favorito.image} nombre={favorito.name} simbolo={favorito.symbol} precio={favorito.current_price} cambio={favorito.price_change_percentage_24h} capitalizacion={favorito.market_cap} id={favorito.id}/>) : 
+          <p className='fav-text'>No tienes favoritos Seleccionados</p> 
           }
         </div>
       </div>
